3d23dba2a2462f03d7e450b1d49dbb0342cbd5d9
igomimu <lucky.mim@gmail.com>
2025-11-17 12:31:06 +0900

    chore: add github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   appType: 'spa',
+  base: process.env.VITE_BASE_PATH || '/',
   server: {
     host: '0.0.0.0',
     port: 5173
